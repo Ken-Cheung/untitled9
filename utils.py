006c76e202e2bb46cce91bf0f38e5a40076043e6
jenkins <jenkins@lazyaudio.com>
2021-06-18 16:22:54 +0800

    修改checkDevice函数，新增对不在ini文件的手机设备判断为设备名叫other

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 # @Time : 2021/6/11 11:52
 
 import configparser
+import os
 import time
 
 from airtest.core.android.adb import ADB
@@ -12,19 +13,21 @@ from airtest.core.android.adb import ADB
 公共工具文件，包含了该自动化的公共函数
 '''
 
+
 def deList():
-    #返回当前设备连接UUID
+    # 返回当前设备连接UUID
     deviceName = []
     connectDevices = ADB().devices('device')
     deviceslist = [i[0] for i in connectDevices]
     return deviceslist
 
+
 def loadini():
-    #读取设备配置ini文件
+    # 读取设备配置ini文件
     try:
         cg = configparser.ConfigParser()
-        cg.read('deviceList.ini')
-        #print(cg.items)
+        cg.read('device.ini')
+        # print(cg.items)
         section_list = cg.items('device')
     except Exception:
         print('读取手机配置文件deviceList.ini出错')
@@ -33,13 +36,18 @@ def loadini():
 
 
 def checkDevice():
-    #将设备保存为 dict
+    # 将设备保存为 dict
     deviceName = []
-    deviceslist =deList()
+    deviceslist = deList()
     section_list = loadini()
     dct = dict(section_list)
-    #翻转 dict
-    new_dict = {v : k for k, v in dct.items()}
+    # 翻转dict
+    new_dict = {v: k for k, v in dct.items()}
+    # 不在配置文件中的设备统统设定为Others的型号，安装App Store版本
+    for i in deviceslist:
+        if i not in new_dict:
+            dct['other'] = '{}'.format(i)
+            new_dict['{}'.format(i)] = 'other'
     dctValues = list(dct.values())
     for i in deviceslist:
         for j in dctValues:
@@ -47,15 +55,18 @@ def checkDevice():
                 deviceName.append(new_dict[i])
     settle = []
     for n in deviceName:
-        if n != 'huawei' and 'yyting':
+        print(n)
+        if n != 'huawei' and n != 'yyting' and n != 'other':
             settle.append(n + 'pro')
         else:
             settle.append(n)
     return settle,new_dict
+    # print(settle, "\n", new_dict)
+# checkDevice()
 
 
 def timeout(sum):
-    #sum = 10 设置倒计时时间
+    # sum = 10 设置倒计时时间
     timeflush = 0.25  # 设置屏幕刷新的间隔时间
     for i in range(0, int(sum/timeflush)):
         list = ["\\", "|", "/", "—"]
@@ -63,3 +74,13 @@ def timeout(sum):
         print("\r请稍等 {}".format(list[index]), end="")
         time.sleep(timeflush)
 
+
+def deleteLoaclApkFiles():
+    # 删除apk文件夹下的apk文件
+    dirPath = os.getcwd()
+    dirPath = os.path.join(dirPath,'apk')
+    for root, dirs, files in os.walk(dirPath):
+        for i in files:
+            baseroot = os.path.join(os.getcwd(),'apk')
+            file = os.path.join(baseroot,"{}".format(i))
+            os.remove(file)
